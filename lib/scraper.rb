74597d4a275c06ddfcf69fe88b731aefd2c28452
Sumei Kom <sumeikom@gmail.com>
2021-03-31 01:29:23 -0400

    commit tree 4fd2af5ce6033e6b26b8fbecd0a9ee530a31e671

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".tout__label.heading.heading--level-4") 
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+  puts course.text.strip
+end 
